3b7f689bc1bdc4368bb9adec9c8d5eb3ec817d45
Abir-bhn <abir.bouchennafa@etudiant.univ-rennes1.fr>
2020-10-12 19:46:40 +0200

    création de la classe message

--- app/src/main/java/com/example/gatein/Message.java
+++ app/src/main/java/com/example/gatein/Message.java
@@ -3,12 +3,18 @@ package com.example.gatein;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Message extends AppCompatActivity {
+    private EditText message;
+    private Button ok;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
+        message=(EditText) findViewById(R.id.editText1);
+        ok=(Button) findViewById(R.id.button);
     }
 }

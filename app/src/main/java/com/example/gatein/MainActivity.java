40f43eafbab3c9fe4f7ecc025ed3aab3d6d0000f
mezianekhodja <meziane.khodja@etudiant.univ-rennes1.fr>
2020-10-12 15:18:47 +0200

    activite connexion

--- app/src/main/java/com/example/gatein/MainActivity.java
+++ app/src/main/java/com/example/gatein/MainActivity.java
@@ -2,13 +2,50 @@ package com.example.gatein;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText name;
+    private EditText password;
+    private TextView info;
+    private Button login;
+    private int counter = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name = (EditText) findViewById(R.id.editTextusername);
+        password = (EditText) findViewById(R.id.editTextpassword);
+        info = (TextView)findViewById(R.id.info);
+        login = (Button)findViewById(R.id.buttonlog);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(name.getText().toString(),password.getText().toString());
+            }
+        });
+    }
+
+    private void validate(String username, String password){
+        if((username.equals("Admin"))&&(password.equals("mdp"))){
+            Intent intent = new Intent(this, Selection.class);
+            startActivity(intent);
+        }
+        else{
+            counter--;
+            info.setText("nombre d'essais restants = "+String.valueOf(counter));
+        }
+        if(counter == 0){
+            login.setEnabled(false);
+        }
     }
 }

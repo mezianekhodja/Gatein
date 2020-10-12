00ff77eb5e97e54c76e5e089f25408efc439f0c4
BlancheLB <blanche.lebonniec@etudiant.univ-rennes1.fr>
2020-10-12 16:44:57 +0200

    modif selection

--- app/src/main/java/com/example/gatein/SelectionGatein.java
+++ app/src/main/java/com/example/gatein/SelectionGatein.java
@@ -3,12 +3,17 @@ package com.example.gatein;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class SelectionGatein extends AppCompatActivity {
+    private Button Ouvrir;
+    private Button Historique;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selection_gatein);
+        Ouvrir=(Button)findViewById(R.id.buttonOuvrir);
+        Historique=(Button)findViewById(R.id.buttonHistorique);
     }
 }
